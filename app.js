33d594c40c034372ec4ed0738dce0ae1540f15e0
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-07-23 17:20:30 -0500

    added type of button

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// console.log('Dad Jokes Starter');
+
 const url = 'https://icanhazdadjoke.com/';
 
-console.log('Dad Jokes Starter');
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+console.log(btn,result);
+
